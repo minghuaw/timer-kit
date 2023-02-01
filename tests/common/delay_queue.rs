1729e180c8e7055bfd62f65ab00f7f4896d9fe91
minghuaw <michael.wu1107@gmail.com>
2023-01-31 18:28:28 -0800

    delay queue tests failing

--- tests/common/delay_queue.rs
+++ tests/common/delay_queue.rs
@@ -268,3 +268,433 @@ where
     let entry = assert_ready_some!(queue.next()).into_inner();
     assert_eq!(entry, "bar");
 }
+
+pub async fn multi_reset<D>() 
+where
+    D: Delay,
+    D::Instant: Unpin,
+{
+    let start = D::Instant::now();
+    let mut queue = timer_kit::DelayQueue::<D, _>::new();
+
+    let key1 = queue.insert_at("one", start + Duration::from_millis(100));
+    let key2 = queue.insert_at("two", start + Duration::from_millis(200));
+
+    assert_pending!(queue.next());
+
+    queue.reset_at(&key1, start + Duration::from_millis(300));
+    queue.reset_at(&key2, start + Duration::from_millis(400));
+    queue.reset_at(&key1, start + Duration::from_millis(500));
+
+    timer_kit::sleep::<D>(Duration::from_millis(110)).await;
+    assert_pending!(queue.next());
+
+    timer_kit::sleep::<D>(Duration::from_millis(300)).await;
+    let entry = assert_ready_some!(queue.next());
+    assert_eq!(entry.into_inner(), "two");
+
+    timer_kit::sleep::<D>(Duration::from_millis(100));
+    let entry = assert_ready_some!(queue.next());
+    assert_eq!(entry.into_inner(), "one");
+}
+
+pub async fn expire_first_key_when_reset_to_expire_earlier<D>()
+where
+    D: Delay,
+    D::Instant: Unpin,
+{
+    let mut queue = timer_kit::DelayQueue::<D, _>::new();
+
+    let start = D::Instant::now();
+
+    let key1 = queue.insert_at("one", start + Duration::from_millis(100));
+    queue.insert_at("two", start + Duration::from_millis(200));
+
+    assert_pending!(queue.next());
+
+    queue.reset_at(&key1, start + Duration::from_millis(50));
+
+    timer_kit::sleep::<D>(Duration::from_millis(60)).await;
+
+    let entry = assert_ready_some!(queue.next());
+
+    assert_eq!(entry.into_inner(), "one");
+}
+
+pub async fn expire_second_key_when_reset_to_expire_earlier<D>()
+where
+    D: Delay,
+    D::Instant: Unpin,
+{
+    let mut queue = timer_kit::DelayQueue::<D, _>::new();
+
+    let start = D::Instant::now();
+
+    queue.insert_at("one", start + Duration::from_millis(100));
+    let key2 = queue.insert_at("two", start + Duration::from_millis(200));
+
+    assert_pending!(queue.next());
+
+    queue.reset_at(&key2, start + Duration::from_millis(50));
+
+    timer_kit::sleep::<D>(Duration::from_millis(60)).await;
+
+    let entry = assert_ready_some!(queue.next());
+
+    assert_eq!(entry.into_inner(), "two");
+}
+
+pub async fn reset_first_expiring_item_to_expire_later<D>()
+where
+    D: Delay,
+    D::Instant: Unpin,
+{
+    let mut queue = timer_kit::DelayQueue::<D, _>::new();
+
+    let start = D::Instant::now();
+
+    let key1 = queue.insert_at("one", start + Duration::from_millis(100));
+    queue.insert_at("two", start + Duration::from_millis(200));
+
+    assert_pending!(queue.next());
+
+    queue.reset_at(&key1, start + Duration::from_millis(300));
+
+    timer_kit::sleep::<D>(Duration::from_millis(210)).await;
+
+    let entry = assert_ready_some!(queue.next());
+
+    assert_eq!(entry.into_inner(), "two");
+}
+
+pub async fn insert_before_first_after_poll<D>()
+where
+    D: Delay,
+    D::Instant: Unpin,
+{
+    let mut queue = timer_kit::DelayQueue::<D, _>::new();
+
+    queue.insert("one", Duration::from_millis(200));
+
+    assert_pending!(queue.next());
+
+    queue.insert("two", Duration::from_millis(100));
+
+    timer_kit::sleep::<D>(Duration::from_millis(99)).await;
+
+    assert_pending!(queue.next());
+
+    timer_kit::sleep::<D>(Duration::from_millis(2)).await;
+
+    let entry = assert_ready_some!(queue.next());
+    assert_eq!(entry.into_inner(), "two");
+}
+
+pub async fn insert_after_ready_poll<D>() 
+where
+    D: Delay,
+    D::Instant: Unpin,
+{
+    let mut queue = timer_kit::DelayQueue::<D, _>::new();
+
+    queue.insert("one", Duration::from_millis(100));
+
+    timer_kit::sleep::<D>(Duration::from_millis(101)).await;
+
+    let entry = assert_ready_some!(queue.next());
+    assert_eq!(entry.into_inner(), "one");
+
+    queue.insert("two", Duration::from_millis(100));
+}
+
+pub async fn reset_later_after_slot_starts<D>()
+where
+    D: Delay,
+    D::Instant: Unpin,
+{
+    let mut queue = timer_kit::DelayQueue::<D, _>::new();
+
+    let start = D::Instant::now();
+
+    let key1 = queue.insert_at("one", start + Duration::from_millis(100));
+
+    assert_pending!(queue.next());
+
+    timer_kit::sleep::<D>(Duration::from_millis(80)).await;
+
+    queue.reset_at(&key1, start + Duration::from_millis(120));
+
+    assert_pending!(queue.next());
+
+    timer_kit::sleep_until::<D>(start + Duration::from_millis(119)).await;
+
+    assert_pending!(queue.next());
+
+    timer_kit::sleep::<D>(Duration::from_millis(2)).await;
+
+    let entry = assert_ready_some!(queue.next());
+    assert_eq!(entry.into_inner(), "one");
+}
+
+pub async fn reset_insert_expired<D>()
+where
+    D: Delay,
+    D::Instant: Unpin,
+{
+    let mut queue = timer_kit::DelayQueue::<D, _>::new();
+
+    let start = D::Instant::now();
+
+    let key = queue.insert_at("one", start - Duration::from_millis(100));
+
+    queue.reset_at(&key, start + Duration::from_millis(100));
+
+    assert_eq!(queue.len(), 1);
+
+    timer_kit::sleep::<D>(Duration::from_millis(101)).await;
+
+    let entry = assert_ready_some!(queue.next());
+    assert_eq!(entry.into_inner(), "one");
+
+    assert_eq!(queue.len(), 0);
+}
+
+pub async fn reset_earlier_after_slot_starts<D>()
+where
+    D: Delay,
+    D::Instant: Unpin,
+{
+    let mut queue = timer_kit::DelayQueue::<D, _>::new();
+
+    let start = D::Instant::now();
+
+    let key1 = queue.insert_at("one", start + Duration::from_millis(200));
+
+    assert_pending!(queue.next());
+
+    timer_kit::sleep::<D>(Duration::from_millis(80)).await;
+
+    queue.reset_at(&key1, start + Duration::from_millis(120));
+
+    assert_pending!(queue.next());
+
+    timer_kit::sleep_until::<D>(start + Duration::from_millis(119)).await;
+
+    assert_pending!(queue.next());
+
+    timer_kit::sleep::<D>(Duration::from_millis(2)).await;
+
+    let entry = assert_ready_some!(queue.next());
+    assert_eq!(entry.into_inner(), "one");
+}
+
+pub async fn insert_in_past_after_poll_fires_immediately<D>()
+where
+    D: Delay,
+    D::Instant: Unpin,
+{
+    let mut queue = timer_kit::DelayQueue::<D, _>::new();
+
+    let now = D::Instant::now();
+
+    queue.insert_at("foo", now + Duration::from_millis(200));
+
+    assert_pending!(queue.next());
+
+    timer_kit::sleep::<D>(Duration::from_millis(80)).await;
+
+    assert_pending!(queue.next());
+    queue.insert_at("bar", now + Duration::from_millis(40));
+
+    let entry = assert_ready_some!(queue.next()).into_inner();
+    assert_eq!(entry, "bar");
+}
+
+pub async fn delay_queue_poll_expired_when_empty<D>()
+where
+    D: Delay,
+    D::Instant: Unpin,
+{
+    let mut queue = timer_kit::DelayQueue::<D, _>::new();
+    let key = queue.insert("foo", Duration::from_millis(100));
+    assert_pending!(queue.next());
+
+    queue.remove(&key);
+    assert_ready_none!(queue.next());
+}
+
+pub async fn compact_expire_empty<D>()
+where
+    D: Delay,
+    D::Instant: Unpin,
+{
+    let mut queue = timer_kit::DelayQueue::<D, _>::new();
+
+    let start = D::Instant::now();
+
+    queue.insert_at("one", start + Duration::from_millis(100));
+    queue.insert_at("two", start + Duration::from_millis(100));
+
+    timer_kit::sleep::<D>(Duration::from_millis(101)).await;
+
+    for _ in 0..2 {
+        assert_ready_some!(queue.next());
+    }
+
+    queue.compact();
+
+    assert_eq!(queue.len(), 0);
+    assert_eq!(queue.capacity(), 0);
+}
+
+pub async fn compact_remove_empty<D>()
+where
+    D: Delay,
+    D::Instant: Unpin,
+{
+    let mut queue = timer_kit::DelayQueue::<D, _>::new();
+
+    let start = D::Instant::now();
+
+    let key1 = queue.insert_at("one", start + Duration::from_millis(100));
+    let key2 = queue.insert_at("two", start + Duration::from_millis(100));
+
+    queue.remove(&key1);
+    queue.remove(&key2);
+
+    queue.compact();
+
+    assert_eq!(queue.len(), 0);
+    assert_eq!(queue.capacity(), 0);
+}
+
+pub async fn compact_remove_remapped_keys<D>()
+where
+    D: Delay,
+    D::Instant: Unpin,
+{
+    let mut queue = timer_kit::DelayQueue::<D, _>::new();
+
+    let start = D::Instant::now();
+
+    queue.insert_at("one", start + Duration::from_millis(100));
+    queue.insert_at("two", start + Duration::from_millis(100));
+
+    let key3 = queue.insert_at("three", start + Duration::from_millis(200));
+    let key4 = queue.insert_at("four", start + Duration::from_millis(200));
+
+    timer_kit::sleep::<D>(Duration::from_millis(101)).await;
+
+    for _ in 0..2 {
+        assert_ready_some!(queue.next());
+    }
+
+    queue.compact();
+
+    queue.insert_at("five", start + Duration::from_millis(100));
+
+    let removed3 = queue.remove(&key3);
+    let removed4 = queue.remove(&key4);
+
+    assert_eq!(removed3.into_inner(), "three");
+    assert_eq!(removed4.into_inner(), "four");
+
+    queue.compact();
+
+    assert_eq!(queue.len(), 1);
+    assert_eq!(queue.capacity(), 1);
+}
+
+pub async fn compact_change_deadline<D>()
+where
+    D: Delay,
+    D::Instant: Unpin,
+{
+    let mut queue = timer_kit::DelayQueue::<D, _>::new();
+
+    let start = D::Instant::now();
+
+    queue.insert_at("one", start + Duration::from_millis(100));
+    queue.insert_at("two", start + Duration::from_millis(100));
+
+    queue.insert_at("three", start + Duration::from_millis(200));
+    let key4 = queue.insert_at("four", start + Duration::from_millis(200));
+
+    timer_kit::sleep::<D>(Duration::from_millis(101)).await;
+
+    for _ in 0..2 {
+        assert_ready_some!(queue.next());
+    }
+
+    queue.compact();
+
+    let start = D::Instant::now();
+    
+    queue.insert_at("five", start + Duration::from_millis(100));
+    let key6 = queue.insert_at("six", start + Duration::from_millis(100));
+
+    queue.reset_at(&key4, start + Duration::from_millis(200));
+    queue.reset_at(&key6, start + Duration::from_millis(200));
+
+    timer_kit::sleep::<D>(Duration::from_millis(101)).await;
+
+    for _ in 0..2 {
+        assert_ready_some!(queue.next());
+    }
+
+    timer_kit::sleep::<D>(Duration::from_millis(101)).await;
+
+    for _ in 0..2 {
+        assert_ready_some!(queue.next());
+    }
+
+    assert_ready_none!(queue.next());
+}
+
+pub async fn remove_after_compact<D>()
+where
+    D: Delay,
+    D::Instant: Unpin,
+{
+    let start = D::Instant::now();
+    let mut queue = timer_kit::DelayQueue::<D, _>::new();
+    let key = queue.insert_at("one", start + Duration::from_millis(100));
+    queue.insert_at("two", start + Duration::from_millis(100));
+    queue.remove(&key);
+}
+
+pub async fn panic_on_remove_of_nonexistent_key<D>()
+where
+    D: Delay,
+    D::Instant: Unpin,
+{
+    let start = D::Instant::now();
+    let mut queue = timer_kit::DelayQueue::<D, _>::new();
+    let key = queue.insert_at("one", start + Duration::from_millis(100));
+    queue.insert_at("two", start + Duration::from_millis(100));
+    queue.remove(&key);
+
+    // This should panic
+    queue.remove(&key);
+}
+
+pub async fn panic_on_remove_after_compact_poll<D>()
+where
+    D: Delay,
+    D::Instant: Unpin,
+{
+    let start = D::Instant::now();
+    let mut queue = timer_kit::DelayQueue::<D, _>::new();
+    let key1 = queue.insert_at("one", start + Duration::from_millis(100));
+    queue.insert_at("two", start + Duration::from_millis(200));
+
+    timer_kit::sleep::<D>(Duration::from_millis(101)).await;
+
+    let entry = assert_ready_some!(queue.next());
+    assert_eq!(entry.into_inner(), "one");
+
+    queue.compact();
+
+    // This should panic
+    queue.remove(&key1);
+}
--- tests/tokio_delay_queue.rs
+++ tests/tokio_delay_queue.rs
@@ -68,4 +68,96 @@ cfg_tokio! {
     async fn expires_before_last_insert() {
         common::delay_queue::expires_before_last_insert::<tokio::time::Sleep>().await;
     }
+
+    #[tokio::test]
+    async fn multi_reset() {
+        common::delay_queue::multi_reset::<tokio::time::Sleep>().await;
+    }
+
+    #[tokio::test]
+    async fn expire_first_key_when_reset_to_expire_earlier() {
+        common::delay_queue::expire_first_key_when_reset_to_expire_earlier::<tokio::time::Sleep>().await;
+    }
+
+    #[tokio::test]
+    async fn expire_second_key_when_reset_to_expire_earlier() {
+        common::delay_queue::expire_second_key_when_reset_to_expire_earlier::<tokio::time::Sleep>().await;
+    }
+
+    #[tokio::test]
+    async fn reset_first_expiring_item_to_expire_later() {
+        common::delay_queue::reset_first_expiring_item_to_expire_later::<tokio::time::Sleep>().await;
+    }
+
+    #[tokio::test]
+    async fn insert_before_first_after_poll() {
+        common::delay_queue::insert_before_first_after_poll::<tokio::time::Sleep>().await;
+    }
+
+    #[tokio::test]
+    async fn insert_after_ready_poll() {
+        common::delay_queue::insert_after_ready_poll::<tokio::time::Sleep>().await;
+    }
+
+    #[tokio::test]
+    async fn reset_later_after_slot_starts() {
+        common::delay_queue::reset_later_after_slot_starts::<tokio::time::Sleep>().await;
+    }
+
+    #[tokio::test]
+    async fn reset_insert_expired() {
+        common::delay_queue::reset_insert_expired::<tokio::time::Sleep>().await;
+    }
+
+    #[tokio::test]
+    async fn reset_earlier_after_slot_starts() {
+        common::delay_queue::reset_earlier_after_slot_starts::<tokio::time::Sleep>().await;
+    }
+
+    #[tokio::test]
+    async fn insert_in_past_after_poll_fires_immediately() {
+        common::delay_queue::insert_in_past_after_poll_fires_immediately::<tokio::time::Sleep>().await;
+    }
+
+    #[tokio::test]
+    async fn delay_queue_poll_expired_when_empty() {
+        common::delay_queue::delay_queue_poll_expired_when_empty::<tokio::time::Sleep>().await;
+    }
+
+    #[tokio::test]
+    async fn compact_expire_empty() {
+        common::delay_queue::compact_expire_empty::<tokio::time::Sleep>().await;
+    }
+
+    #[tokio::test]
+    async fn compact_remove_empty() {
+        common::delay_queue::compact_remove_empty::<tokio::time::Sleep>().await;
+    }
+
+    #[tokio::test]
+    async fn compact_remove_remapped_keys() {
+        common::delay_queue::compact_remove_remapped_keys::<tokio::time::Sleep>().await;
+    }
+
+    #[tokio::test]
+    async fn compact_change_deadline() {
+        common::delay_queue::compact_change_deadline::<tokio::time::Sleep>().await;
+    }
+
+    #[tokio::test]
+    async fn remove_after_compact() {
+        common::delay_queue::remove_after_compact::<tokio::time::Sleep>().await;
+    }
+
+    #[tokio::test]
+    #[should_panic]
+    async fn panic_on_remove_of_nonexistent_key() {
+        common::delay_queue::panic_on_remove_of_nonexistent_key::<tokio::time::Sleep>().await;
+    }
+
+    #[tokio::test]
+    #[should_panic]
+    async fn panic_on_remove_after_compact_poll() {
+        common::delay_queue::panic_on_remove_after_compact_poll::<tokio::time::Sleep>().await;
+    }
 }
